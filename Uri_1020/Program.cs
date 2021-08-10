24b617645061b621f8da9b782e4eed735a997deb
RaulORios <raul.orios89@outlook.com>
2021-08-10 12:03:20 -0300

    Arquivos atualizados

---
+++ Uri_1020/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Uri_1020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, quociente, resto;
+            n = int.Parse(Console.ReadLine());
+
+            resto = n;
+
+            quociente = resto / 365;
+            Console.WriteLine($"{quociente} ano(s)");
+            resto = resto % 365;
+
+            quociente = resto / 30;
+            Console.WriteLine($"{quociente} mes(es)");
+            resto = resto % 30;
+
+            Console.WriteLine($"{resto} dia(s)");
+
+        }
+    }
+}
